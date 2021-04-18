27a1fc4ef2a7b8723cc26b5af41b119c4d3756ea
stockbal <stocki87@gmail.com>
2021-04-18 20:28:02 +0200

    feat: remove tab layout

--- webapp/controller/MainPage.controller.js
+++ webapp/controller/MainPage.controller.js
@@ -32,102 +32,8 @@ sap.ui.define(
                 });
             },
 
-            onCloseAnalysis(event) {},
-
-            onCloseAllEntities(event) {
-                const openAnalysesToClose = [];
-                const tabBar = this._getAnalysisTabBar();
-                const items = tabBar.getItems();
-                items?.forEach(item => {
-                    if (!(item instanceof IconTabFilter)) {
-                        return;
-                    }
-                    if (item.getKey().startsWith(ANALYSIS_FILTER_PREFIX)) {
-                        openAnalysesToClose.push(item.getId());
-                    }
-                });
-                const closeItems = () => {
-                    openAnalysesToClose.forEach(id => {
-                        tabBar.removeItem(id);
-                    });
-                    this._openEntityMap.clear();
-                };
-                if (openAnalysesToClose.length > 0) {
-                    const bundle = this.getResourceBundle();
-                    MessageBox.confirm(bundle.getText("message_question_closeAllTabs"), {
-                        title: bundle.getText("message_title_confirm"),
-                        onClose: actionId => {
-                            if (actionId === MessageBox.Action.OK) {
-                                closeItems();
-                            }
-                        }
-                    });
-                }
-            },
-
-            /**
-             * Opens pressed db entity as new "tab" in icon tab
-             * @param {Object} event press event object
-             */
-            onOpenEntity(event) {
-                const tabBar = this._getAnalysisTabBar();
-                const item = event.getSource();
-                const dbEntity = this._viewModel.getObject(item.getBindingContextPath());
-                if (dbEntity) {
-                    const newEntityKey = `${ANALYSIS_FILTER_PREFIX}${dbEntity.name}:${Date.now()}`;
-                    const newEntityTab = new IconTabFilter({
-                        design: sap.m.IconTabFilterDesign.Horizontal,
-                        key: newEntityKey,
-                        text: dbEntity.name,
-                        icon: dbEntity.typeIcon,
-                        tooltip: `${dbEntity.name}\n${dbEntity.description}`
-                    });
-                    // attach browser event to show custom context menu to tab filter
-                    newEntityTab.addEventDelegate({
-                        oncontextmenu: e => {
-                            e.preventDefault();
-                            const bundle = this.getResourceBundle();
-                            const menu = new sap.m.Menu({
-                                items: [
-                                    new sap.m.MenuItem({
-                                        icon: "sap-icon://refresh",
-                                        text: bundle.getText("analysisTab_contextmenu_refresh")
-                                    }),
-                                    new sap.m.MenuItem({
-                                        icon: "sap-icon://decline",
-                                        text: bundle.getText("analysisTab_contextmenu_close"),
-                                        startsSection: true,
-                                        press: e => {
-                                            tabBar.removeItem(newEntityTab);
-                                            this._openEntityMap.delete(newEntityKey);
-                                        }
-                                    })
-                                ]
-                            });
-                            menu.openBy(newEntityTab);
-                        }
-                    });
-                    tabBar.addItem(newEntityTab);
-                    this._openEntityMap.set(newEntityKey, {
-                        name: dbEntity.name,
-                        type: dbEntity.type,
-                        description: dbEntity.description,
-                        packageName: dbEntity.packageName
-                    });
-
-                    tabBar.setSelectedKey(newEntityKey);
-
-                    // update current entity property
-                    const entityInfo = this._openEntityMap.get(newEntityKey);
-                    this._viewModel.setProperty("/currentEntity", entityInfo);
-                }
-            },
             onEntitySelect(event) {
                 const entityKey = event.getParameter("key");
-                if (entityKey) {
-                    const entityInfo = this._openEntityMap.get(entityKey);
-                    this._viewModel.setProperty("/currentEntity", entityInfo);
-                }
             },
 
             async onSearchForEntities(event) {
@@ -173,11 +79,7 @@ sap.ui.define(
                 } else {
                     this._viewModel.setProperty("/foundEntities", []);
                 }
-            },
-            _getAnalysisTabBar() {
-                return this.getView().byId("analysisTabBar");
-            },
-            _getFreeEntityKey(entityName) {}
+            }
         });
     }
 );
