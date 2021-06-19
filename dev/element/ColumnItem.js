a0dbee01286fe287bfcb02f763bdf97ba77cbe9d
stockbal <stocki87@gmail.com>
2021-06-19 17:19:56 +0200

    refactor: move classes derived from element into element folder

---
+++ dev/element/ColumnItem.js
@@ -0,0 +1,11 @@
+import Item from "sap/ui/core/Item";
+
+/**
+ * Item which describes a column configuration
+ * @namespace devepos.qdrt.element
+ */
+export default class ColumnItem extends Item {
+    metadata = {
+        properties: {}
+    };
+}
--- dev/element/FilterItem.js
+++ dev/element/FilterItem.js
@@ -1,11 +1,11 @@
-import Element from "sap/ui/core/Element";
+import Item from "sap/ui/core/Item";
 
 /**
  * Holds configuration for a DB entity filter
  *
  * @namespace devepos.qdrt.type
  */
-export default class FilterConfig extends Element {
+export default class FilterItem extends Item {
     metadata = {
         properties: {}
     };
