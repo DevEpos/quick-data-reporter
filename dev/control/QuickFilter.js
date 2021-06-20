9fd624fd9da1bffbd3083acf2fc444a1e71b83ca
stockbal <stocki87@gmail.com>
2021-06-20 15:24:50 +0200

    feat: enhance side filter bar controls

--- dev/control/QuickFilter.js
+++ dev/control/QuickFilter.js
@@ -1,4 +1,12 @@
 import Control from "sap/ui/core/Control";
+import Button from "sap/m/Button";
+import MultiInput from "sap/m/MultiInput";
+import Label from "sap/m/Label";
+import Text from "sap/m/Text";
+import ToolbarSpacer from "sap/m/ToolbarSpacer";
+import Title from "sap/m/Title";
+import FlexBox from "sap/m/FlexBox";
+import VerticalLayout from "sap/ui/layout/VerticalLayout";
 
 /**
  * Quick Filter in {@link devepos.qdrt.control.SideFilterBar}
@@ -7,17 +15,58 @@ import Control from "sap/ui/core/Control";
  */
 export default class QuickFilter extends Control {
     metadata = {
-        properties: {},
-        defaultAggregation: "content",
+        properties: {
+            columnName: {
+                type: "string",
+                group: "Misc",
+                defaultValue: null
+            }
+        },
         aggregations: {
             /**
              * Control for the filter
              */
-            content: { type: "sap.ui.core.Control", multiple: true, singularName: "content" }
+            filter: { type: "sap.ui.core.Control", multiple: false, singularName: "filter", visibility: "hidden" }
         },
         events: {}
     };
-    init() {}
+    renderer = {
+        apiVersion: 2,
+        render(rm, control) {
+            rm.renderControl(control.getAggregation("filter"));
+        }
+    };
+    init() {
+        this._filterName = new Text();
+        const filterCont = new VerticalLayout({
+            content: [
+                new FlexBox({
+                    alignItems: sap.m.FlexAlignItems.Center,
+                    justifyContent: sap.m.FlexJustifyContent.SpaceBetween,
+                    items: [
+                        this._filterName,
+                        new Button({
+                            icon: "sap-icon://decline",
+                            type: sap.m.ButtonType.Transparent,
+                            press: () => {
+                                this.destroy();
+                            }
+                        })
+                    ]
+                }),
+                // TODO: create correct control type
+                new MultiInput({ width: "100%" })
+            ]
+        });
+        filterCont.addStyleClass("deveposQdrtQuickFilter");
+        this.setAggregation("filter", filterCont);
+    }
+    setColumnName(columnName) {
+        this.setProperty("columnName", columnName);
+
+        this._filterName?.setText(columnName);
+        return this;
+    }
     onAfterRendering() {}
     onBeforeRendering() {}
     exit() {}
--- dev/control/SideFilterBar.js
+++ dev/control/SideFilterBar.js
@@ -1,24 +1,96 @@
-import Control from "sap/ui/core/Control";
+import Panel from "sap/m/Panel";
+import ScrollContainer from "sap/m/ScrollContainer";
+import OverflowToolbar from "sap/m/OverflowToolbar";
+import Button from "sap/m/Button";
+import ToolbarSpacer from "sap/m/ToolbarSpacer";
+import Title from "sap/m/Title";
+import VerticalLayout from "sap/ui/layout/VerticalLayout";
+import QuickFilter from "./QuickFilter";
 
 /**
  * FilterBar with vertical orientation
  *
  * @namespace devepos.qdrt.control
  */
-export default class SideFilterBar extends Control {
+export default class SideFilterBar extends Panel {
     metadata = {
         properties: {},
-        defaultAggregation: "filters",
         aggregations: {
             /**
-             * Holds the current filters of the SideFilterBar
+             * Holds all columns for which filters can be created
              */
-            filters: { type: "devepos.qdrt.control.QuickFilter", multiple: true, singularName: "filter" }
+            columnItems: { type: "devepos.qdrt.element.ColumnItem", multiple: true, singularName: "columnItem" },
+            /**
+             * Holds the current filter items
+             */
+            filterItems: { type: "devepos.qdrt.element.FilterItem", multiple: true, singularName: "filterItem" }
         },
         events: {}
     };
-    init() {}
-    onAfterRendering() {}
-    onBeforeRendering() {}
-    exit() {}
+    /**
+     * Currently no custom renderer is needed
+     */
+    renderer = "sap.m.PanelRenderer";
+    init() {
+        Panel.prototype.init.call(this);
+        this._onAfterRenderingFirstTimeExecuted = false;
+        this._liveChangeTimer = 0;
+        this.setWidth("100%");
+        this.setHeight("100%");
+        this.setHeaderToolbar(
+            new OverflowToolbar({
+                content: [
+                    // TODO: create i18n text
+                    new Title({ text: "Filters" }),
+                    new ToolbarSpacer(),
+                    // TODO: add i18n tooltip
+                    new Button({
+                        icon: "sap-icon://add",
+                        type: sap.m.ButtonType.Transparent,
+                        press: () => {
+                            this._addNewFilter();
+                        }
+                    }),
+                    // TODO: add i18n tooltip
+                    new Button({
+                        icon: "sap-icon://delete",
+                        type: sap.m.ButtonType.Transparent,
+                        press: () => {
+                            this._filterContainer.removeAllContent();
+                        }
+                    })
+                ]
+            })
+        );
+        this._filterContainer = new VerticalLayout({
+            width: "100%"
+        });
+        this._scrollContainer = new ScrollContainer({
+            content: this._filterContainer,
+            width: "100%",
+            height: "100%",
+            horizontal: false,
+            vertical: true
+        });
+
+        this.addContent(this._scrollContainer);
+    }
+    onAfterRendering() {
+        // do custom afterRendering
+        Panel.prototype.onAfterRendering.call(this);
+    }
+    onBeforeRendering() {
+        Panel.prototype.onBeforeRendering.call(this);
+        // do custom beforeRendering
+    }
+    exit() {
+        Panel.prototype.exit.call(this);
+    }
+
+    _addNewFilter() {
+        this._filterContainer.addContent(this._createQuickFilter("New Filter"));
+    }
+    _createQuickFilter(columnName) {
+        return new QuickFilter({ columnName });
+    }
 }
