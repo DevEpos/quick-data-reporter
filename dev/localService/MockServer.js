a96099b8062eabd8ca8a46e16c1e28c820deed18
stockbal <stocki87@gmail.com>
2021-06-22 22:56:33 +0200

    feat: add functions to generate random values

--- dev/localService/MockServer.js
+++ dev/localService/MockServer.js
@@ -2,6 +2,8 @@ import MockServer from "sap/ui/core/util/MockServer";
 import Log from "sap/base/Log";
 import UriParameters from "sap/base/util/UriParameters";
 import AjaxUtil from "../model/dataAccess/util/ajaxUtil";
+import DateFormat from "sap/ui/core/format/DateFormat";
+import CalendarType from "sap/ui/core/CalendarType";
 
 let mockServer;
 const _sAppModulePath = "devepos/qdrt/";
@@ -16,12 +18,20 @@ export default {
      *
      */
     init() {
+        this._mockData = {};
         const uriParameters = new UriParameters(window.location.href);
         const mockServerUrl = "/sap/zqdrtrest/";
 
         mockServer = new MockServer({
             rootUri: mockServerUrl
         });
+        this._modelFormatter = DateFormat.getDateInstance({
+            calendarType: CalendarType.Gregorian,
+            pattern: "yyyy-MM-dd",
+            strictParsing: true,
+            UTC: true
+        });
+        this._randomSeed = {};
 
         // configure mock server with a delay of 1s
         MockServer.config({
@@ -29,16 +39,6 @@ export default {
             autoRespondAfter: uriParameters.get("serverDelay") || 1000
         });
 
-        const getJson = (xhr, jsonFileName) => {
-            const localUri = sap.ui.require.toUrl(_sJsonFilesModulePath + jsonFileName + ".json");
-            const json = AjaxUtil.sendSync(localUri);
-            if (json.status === 200) {
-                xhr.respondJSON(200, {}, json.data);
-            } else {
-                xhr.respondJSON(json.status, {}, []);
-            }
-            return true;
-        };
         mockServer.setRequests([
             {
                 method: "HEAD",
@@ -51,21 +51,21 @@ export default {
                 method: "GET",
                 path: /entities\/vh.*/,
                 response: xhr => {
-                    return getJson(xhr, "dbentities");
+                    this._getMockdata(xhr, "dbentities");
                 }
             },
             {
                 method: "POST",
                 path: /entities\/(.*)\/(.*)\/dataPreview.*/,
                 response: xhr => {
-                    return getJson(xhr, "datapreview");
+                    this._getMockdata(xhr, "datapreview");
                 }
             },
             {
                 method: "POST",
                 path: /entities\/(.*)\/(.*)\/metadata.*/,
                 response: xhr => {
-                    return getJson(xhr, "entityMetadata")
+                    this._getMockdata(xhr, "entityMetadata");
                 }
             }
         ]);
@@ -81,5 +81,129 @@ export default {
      */
     getMockServer() {
         return mockServer;
+    },
+
+    _getCachedMockdata(jsonFileName) {
+        if (this._mockData[jsonFileName]) {
+            return this._mockData[jsonFileName];
+        } else {
+            const jsonResponse = this._getJSONContent(jsonFileName);
+            if (jsonResponse?.status === 200) {
+                this._mockData[jsonFileName] = jsonResponse?.data || {};
+            } else {
+                this._mockData[jsonFileName] = null;
+            }
+            return jsonResponse?.data;
+        }
+    },
+
+    _getJSONContent(jsonFileName) {
+        const localUri = sap.ui.require.toUrl(_sJsonFilesModulePath + jsonFileName + ".json");
+        return AjaxUtil.sendSync(localUri);
+    },
+
+    _getMockdata(xhr, jsonFileName) {
+        try {
+            const json = this._getCachedMockdata(jsonFileName);
+            if (json) {
+                xhr.respondJSON(200, {}, json);
+            } else {
+                xhr.respondJSON(204, {}, []);
+            }
+        } catch (errorStatus) {
+            xhr.respondJSON(500, {}, []);
+        }
+    },
+
+    _getPseudoRandomNumber(type) {
+        if (!this._randomSeed) {
+            this._randomSeed = {};
+        }
+        if (!this._randomSeed.hasOwnProperty(type)) {
+            this._randomSeed[type] = 0;
+        }
+        this._randomSeed[type] = ((this._randomSeed[type] + 11) * 25214903917) % 281474976710655;
+        return this._randomSeed[type] / 281474976710655;
+    },
+
+    _generatePropertyValue(propertyName, type, index) {
+        if (!index) {
+            index = Math.floor(this._getPseudoRandomNumber("String") * 10000) + 101;
+        }
+        switch (type) {
+            case "String":
+                return propertyName + " " + index;
+            case "DateTime":
+                var date = new Date();
+                date.setFullYear(2000 + Math.floor(this._getPseudoRandomNumber("DateTime") * 20));
+                date.setDate(Math.floor(this._getPseudoRandomNumber("DateTime") * 30));
+                date.setMonth(Math.floor(this._getPseudoRandomNumber("DateTime") * 12));
+                date.setMilliseconds(0);
+                return "/Date(" + date.getTime() + ")/";
+            case "Date":
+                var date = new Date();
+                date.setFullYear(2000 + Math.floor(this._getPseudoRandomNumber("DateTime") * 20));
+                date.setDate(Math.floor(this._getPseudoRandomNumber("DateTime") * 30));
+                date.setMonth(Math.floor(this._getPseudoRandomNumber("DateTime") * 12));
+                date.setMilliseconds(0);
+                return this._modelFormatter.format(date);
+            case "Int":
+            case "Int16":
+            case "Int32":
+            case "Int64":
+                return Math.floor(this._getPseudoRandomNumber("Int") * 10000);
+            case "Decimal":
+                return Math.floor(this._getPseudoRandomNumber("Decimal") * 1000000) / 100;
+            case "Boolean":
+                return this._getPseudoRandomNumber("Boolean") < 0.5;
+            case "Byte":
+                return Math.floor(this._getPseudoRandomNumber("Byte") * 10);
+            case "Double":
+                return this._getPseudoRandomNumber("Double") * 10;
+            case "Single":
+                return this._getPseudoRandomNumber("Single") * 1000000000;
+            case "SByte":
+                return Math.floor(this._getPseudoRandomNumber("SByte") * 10);
+            case "Time":
+                // ODataModel expects ISO8601 duration format
+                return (
+                    "PT" +
+                    Math.floor(this._getPseudoRandomNumber("Time") * 23) +
+                    "H" +
+                    Math.floor(this._getPseudoRandomNumber("Time") * 59) +
+                    "M" +
+                    Math.floor(this._getPseudoRandomNumber("Time") * 59) +
+                    "S"
+                );
+            case "Guid":
+                return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+                    /[xy]/g,
+                    function (c) {
+                        var r = (this._getPseudoRandomNumber("Guid") * 16) | 0,
+                            v = c === "x" ? r : (r & 0x3) | 0x8;
+                        return v.toString(16);
+                    }.bind(this)
+                );
+            case "Binary":
+                var nMask = Math.floor(-2147483648 + this._getPseudoRandomNumber("Binary") * 4294967295),
+                    sMask = "";
+                /*eslint-disable */
+                for (
+                    var nFlag = 0, nShifted = nMask;
+                    nFlag < 32;
+                    nFlag++, sMask += String(nShifted >>> 31), nShifted <<= 1
+                );
+                /*eslint-enable */
+                return sMask;
+            case "DateTimeOffset":
+                var date = new Date();
+                date.setFullYear(2000 + Math.floor(this._getPseudoRandomNumber("DateTimeOffset") * 20));
+                date.setDate(Math.floor(this._getPseudoRandomNumber("DateTimeOffset") * 30));
+                date.setMonth(Math.floor(this._getPseudoRandomNumber("DateTimeOffset") * 12));
+                date.setMilliseconds(0);
+                return "/Date(" + date.getTime() + "+0000)/";
+            default:
+                return propertyName + " " + index;
+        }
     }
 };
