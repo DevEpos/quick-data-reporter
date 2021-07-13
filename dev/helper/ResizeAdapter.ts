21620fa8f60e6cb4ce1fd4bd0fcc046e0526f783
stockbal <ludwig.stockbauer-muhr@msg.group>
2021-07-13 13:14:18 +0200

    refactor: allow all resizable controls not only scroll containers

--- dev/helper/ResizeAdapter.ts
+++ dev/helper/ResizeAdapter.ts
@@ -1,4 +1,3 @@
-import ScrollContainer from "sap/m/ScrollContainer";
 import Control from "sap/ui/core/Control";
 import ResizeHandler from "sap/ui/core/ResizeHandler";
 
@@ -7,7 +6,7 @@ import ResizeHandler from "sap/ui/core/ResizeHandler";
  * @namespace devepos.qdrt.helper
  */
 export default class ResizeAdapter {
-    _scrollContainer: ScrollContainer;
+    _resizableControl: Control;
     _parentControl: Control;
     _parentControlDomSuffix: string;
     _excludedHeightsControls: Control[];
@@ -17,19 +16,23 @@ export default class ResizeAdapter {
 
     /**
      * Creates new Resize adapter instance
-     * @param {sap.m.ScrollContainer} scrollContainer the scroll container whoose height should be set
-     * @param {sap.m.Control} parentControl the parent container to get the size for the scroll container
-     * @param {string} parentControlDomSuffix optional id suffix to read concrete dom element
-     * @param {sap.m.Control[]} excludedHeightsControls optional array of control whose height should
+     * @param resizableControl the scroll container whoose height should be set
+     * @param parentControl the parent container to get the size for the scroll container
+     * @param parentControlDomSuffix optional id suffix to read concrete dom element
+     * @param excludedHeightsControls optional array of control whose height should
      *      be subtracted from the scroll container height
      */
     constructor(
-        scrollContainer: ScrollContainer,
+        resizableControl: Control,
         parentControl: Control,
         parentControlDomSuffix?: string,
         excludedHeightsControls?: Control[]
     ) {
-        this._scrollContainer = scrollContainer;
+        if (!(resizableControl as any)?.setHeight) {
+            throw Error("Control is not resizable via 'setHeight'");
+        }
+        this._resizableControl = resizableControl;
+
         this._parentControl = parentControl;
         this._parentControlDomSuffix = parentControlDomSuffix;
         this._excludedHeightsControls = excludedHeightsControls;
@@ -58,20 +61,20 @@ export default class ResizeAdapter {
     }
     private _onResize(): boolean {
         let resultChanged = false;
-        let oldScrollContainerHeight;
-        let newScrollContainerHeight;
+        let oldControlHeight;
+        let newControlHeight;
 
         if (this._parentControl) {
-            oldScrollContainerHeight = this._scrollContainer.$().outerHeight();
-            newScrollContainerHeight = this._parentControl.$(this._parentControlDomSuffix).height();
+            oldControlHeight = this._resizableControl.$().outerHeight();
+            newControlHeight = this._parentControl.$(this._parentControlDomSuffix).height();
             if (this._excludedHeightsControls?.length > 0) {
                 for (const excludeHeightControl of this._excludedHeightsControls) {
-                    newScrollContainerHeight -= excludeHeightControl ? excludeHeightControl.$().outerHeight() : 0;
+                    newControlHeight -= excludeHeightControl ? excludeHeightControl.$().outerHeight() : 0;
                 }
             }
 
-            if (oldScrollContainerHeight !== newScrollContainerHeight) {
-                this._scrollContainer.setHeight(`${newScrollContainerHeight}px`);
+            if (oldControlHeight !== newControlHeight) {
+                (this._resizableControl as any).setHeight(`${newControlHeight}px`);
                 resultChanged = true;
             }
         }
